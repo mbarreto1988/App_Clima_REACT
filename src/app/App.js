5d393bb92146010164f8903bd1c39bfb730a0ef3
Matias <barretomatiasgabriel@gmail.com>
2022-11-02 09:48:20 -0300

    consumiendo Apis

--- src/app/App.js
+++ src/app/App.js
@@ -26,17 +26,69 @@ class App extends Component {
         if(cityValue && countryValue){
 
             const API_URL = `https://api.openweathermap.org/data/2.5/weather?q=${cityValue},ar&appid=${WEATHER_KEY}&units=metric`;
+            const api_dias = `https://api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid=ed19f75d2b20a8a8c280df206dcb079a&units=metric`;
             const response = await fetch(API_URL);
             const data = await response.json();
+
+            const respuesta1 = await fetch(api_dias);
+            const datoss = await respuesta1.json();
+            console.log(datoss);
+            console.log(datoss.list[0].dt_txt);
+            console.log(datoss.list[4].dt_txt);
+            console.log(datoss.list[12].dt_txt);
+            console.log(datoss.list[19].dt_txt);
+            console.log(datoss.list[26].dt_txt);
+            console.log(datoss.list[35].dt_txt);
     
             this.setState({
+                day: datoss.list[0].dt_txt,
                 temperature: data.main.temp,
                 description: data.weather[0].description,
                 humidity: data.main.humidity,
                 wind_speed: data.wind.speed,
                 city: data.name,
                 country: data.sys.country,
-                error: null
+                error: null,
+                day_2: datoss.list[4].dt_txt,
+                temperature_2: datoss.list[4].main.temp,
+                description_2: datoss.list[4].weather[0].description,
+                humidity_2: datoss.list[4].main.humidity,
+                wind_speed_2: datoss.list[4].wind.speed,
+                city_2: datoss.list[4].name,
+                country_2: datoss.list[4].sys.country,
+                error_2: null,
+                day_3: datoss.list[12].dt_txt,
+                temperature_3: datoss.list[12].main.temp,
+                description_3: datoss.list[12].weather[0].description,
+                humidity_3: datoss.list[12].main.humidity,
+                wind_speed_3: datoss.list[12].wind.speed,
+                city_3: datoss.list[12].name,
+                country_3: datoss.list[12].sys.country,
+                error_3: null,
+                day_4: datoss.list[19].dt_txt,
+                temperature_4: datoss.list[19].main.temp,
+                description_4: datoss.list[19].weather[0].description,
+                humidity_4: datoss.list[19].main.humidity,
+                wind_speed_4: datoss.list[19].wind.speed,
+                city_4: datoss.list[19].name,
+                country_4: datoss.list[19].sys.country,
+                error_4: null,
+                day5: datoss.list[26].dt_txt,
+                temperature5: datoss.list[26].main.temp,
+                description5: datoss.list[26].weather[0].description,
+                humidity5: datoss.list[26].main.humidity,
+                wind_speed5: datoss.list[26].wind.speed,
+                city5: datoss.list[26].name,
+                country5: datoss.list[26].sys.country,
+                error5: null,                
+                day_6: datoss.list[35].dt_txt,
+                temperature_6: datoss.list[35].main.temp,
+                description_6: datoss.list[35].weather[0].description,
+                humidity_6: datoss.list[35].main.humidity,
+                wind_speed_6: datoss.list[35].wind.speed,
+                city_6: datoss.list[35].name,
+                country5: datoss.list[35].sys.country,
+                error5: null
             });
         } else {
             this.setState({error: 'Please enter a City and a Country.'})
--- src/app/components/WeatherInfo.js
+++ src/app/components/WeatherInfo.js
@@ -4,14 +4,17 @@ const WeatherInfo = props => {
     console.log(props)
     return (
         <div>
-            {
-                props.error &&
+            {props.error &&
                 <div className="alert alert-danger">
                     <p>{props.error}</p>
                 </div>
             }
             {props.temperature ?
                 <div className="card card-body mt-2 animated fadeInUp" >
+                    {
+                        props.day &&
+                        <p><i class="fas fa-calendar"></i> Día: {props.day}, {props.country}</p>
+                    }
                     {
                         props.city && props.country &&
                         <p><i className="fas fa-location-arrow"></i> Ubicacion: {props.city}, {props.country}</p>
@@ -22,20 +25,69 @@ const WeatherInfo = props => {
                     }
                     {
                         props.humidity &&
-                        <p><i className="fas fa-water"></i> Humedad: {props.humidity}</p>
+                        <p><i className="fas fa-water"></i> Humedad: {props.humidity} %</p>
                     }
                     {
                         props.wind_speed &&
-                        <p><i className="fas fa-wind"></i> Velocidad del Viento: {props.wind_speed}</p>
+                        <p><i className="fas fa-wind"></i> Velocidad del Viento: {props.wind_speed} Km/h</p>
                     }
-                </div>
+                </div>                
                 :
                 <div className="card card-body mt-2 text-center">
-                    <i className="fas fa-sun fa-10x"></i>
+                    <i className="fas fa-sun fa-8x"></i>
                 </div>
             }
+            {props.city ?
+                <div className="card card-body mt-2 animated fadeInUp" >
+                    {
+                        props.day_2 &&
+                        <p><i class="fas fa-calendar"></i> Día: {props.day_2}, {props.country}</p>
+                    }
+                    {
+                        props.city && props.country &&
+                        <p><i className="fas fa-location-arrow"></i> Ubicacion: {props.city}, {props.country}</p>
+                    }
+                    {
+                        props.temperature_2 &&
+                        <p><i className="fas fa-temperature-low"></i> Temperatura: {props.temperature_2} ℃, {props.description}</p>
+                    }
+                    {
+                        props.humidity_2 &&
+                        <p><i className="fas fa-water"></i> Humedad: {props.humidity_2} %</p>
+                    }
+                    {
+                        props.wind_speed_2 &&
+                        <p><i className="fas fa-wind"></i> Velocidad del Viento: {props.wind_speed_2} Km/h</p>
+                    }
+                </div>                
+                : ""
+            }
+            {props.city ?
+                <div className="card card-body mt-2 animated fadeInUp" >
+                    {
+                        props.day_2 &&
+                        <p><i class="fas fa-calendar"></i> Día: {props.day_2}, {props.country}</p>
+                    }
+                    {
+                        props.city && props.country &&
+                        <p><i className="fas fa-location-arrow"></i> Ubicacion: {props.city}, {props.country}</p>
+                    }
+                    {
+                        props.temperature_2 &&
+                        <p><i className="fas fa-temperature-low"></i> Temperatura: {props.temperature_2} ℃, {props.description}</p>
+                    }
+                    {
+                        props.humidity_2 &&
+                        <p><i className="fas fa-water"></i> Humedad: {props.humidity_2} %</p>
+                    }
+                    {
+                        props.wind_speed_2 &&
+                        <p><i className="fas fa-wind"></i> Velocidad del Viento: {props.wind_speed_2} Km/h</p>
+                    }
+                </div>                
+                : ""
+            }
         </div>
-
     )
 }
 
